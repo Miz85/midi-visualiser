f6e6fccf0a6383bc7e49d3620d3e44d58c14eb73
Nazim Saouli <nazim.saouli@datadoghq.com>
2026-02-22 21:45:11 +0100

    Deps update

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
-import ReactComponentName from "react-scan/react-component-name/webpack";
-
 /** @type {import("next").NextConfig} */
 const config = {
-    // Only needs to be enabled in production. 
-    // If you're using Turborepo, you should disable React Component Name in development.
-    webpack: (config) => {
-        config.plugins.push(ReactComponentName({}));
-        return config;
-    },
+    /* Turbopack est utilisé par défaut dans Next.js 16 */
 };
 
 export default config;
